c15b6d5d4483a56a5c3268a94e43dee2889615ae
bnj997 <bnj997@gmail.com>
2020-09-28 12:15:29 +1000

    changed port

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ mongoose
     `mongodb+srv://${NAME}:${PASSWORD}@business-apparel.fccf2.mongodb.net/business_apparel?retryWrites=true&w=majority`, { useUnifiedTopology: true, useNewUrlParser: true}
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch(err => {
     console.log(err);

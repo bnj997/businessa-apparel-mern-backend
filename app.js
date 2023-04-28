5d9b52c85414c818a59852bb7b7d701368e438df
bnj997 <bnj997@gmail.com>
2023-04-28 21:03:35 +1000

    minor change

--- app.js
+++ app.js
@@ -1,44 +1,40 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const HttpError = require('./models/http-error');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const HttpError = require("./models/http-error");
 
-const branchesRoutes = require('./routes/branches-routes');
-const garmentsRoutes = require('./routes/garments-routes');
-const hqsRoutes = require('./routes/hqs-routes');
-const usersRoutes = require('./routes/users-routes');
-const orderLinesRoutes = require('./routes/order-lines-routes');
-const ordersRoutes = require('./routes/orders-routes');
-
+const branchesRoutes = require("./routes/branches-routes");
+const garmentsRoutes = require("./routes/garments-routes");
+const hqsRoutes = require("./routes/hqs-routes");
+const usersRoutes = require("./routes/users-routes");
+const orderLinesRoutes = require("./routes/order-lines-routes");
+const ordersRoutes = require("./routes/orders-routes");
 
 const app = express();
 
-app.use(bodyParser.json())
-
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
   next();
 });
 
-app.use('/api/branches', branchesRoutes);
-app.use('/api/garments', garmentsRoutes);
-app.use('/api/hqs', hqsRoutes);
-app.use('/api/users', usersRoutes);
-app.use('/api/orders', ordersRoutes);
-app.use('/api/order-lines', orderLinesRoutes);
-
-
+app.use("/api/branches", branchesRoutes);
+app.use("/api/garments", garmentsRoutes);
+app.use("/api/hqs", hqsRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/orders", ordersRoutes);
+app.use("/api/order-lines", orderLinesRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route.', 404);
+  const error = new HttpError("Could not find this route.", 404);
   throw error;
 });
 
@@ -47,20 +43,19 @@ app.use((error, req, res, next) => {
     return next(error);
   }
   res.status(error.code || 500);
-  res.json({ message: error.message || 'An unknown error occurred!' });
+  res.json({ message: error.message || "An unknown error occurred!" });
 });
-
 
 const NAME = process.env.NAME;
 const PASSWORD = process.env.PASSWORD;
 mongoose
   .connect(
-    `mongodb+srv://${NAME}:${PASSWORD}@business-apparel.fccf2.mongodb.net/business_apparel?retryWrites=true&w=majority`, { useUnifiedTopology: true, useNewUrlParser: true}
+    `mongodb+srv://${NAME}:${PASSWORD}@business-apparel.fccf2.mongodb.net/business_apparel?retryWrites=true&w=majority`,
+    { useUnifiedTopology: true, useNewUrlParser: true }
   )
   .then(() => {
     app.listen(process.env.PORT || 5000);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
-
--- utils/check-permission.js
+++ utils/check-permission.js
@@ -1,10 +1,10 @@
-const HttpError = require('../models/http-error');
+const HttpError = require("../models/http-error");
 
 const checkPermission = async (username, next) => {
   if (username !== "adminstaff") {
-    const error = new HttpError('Unauthorised action.', 401);
+    const error = new HttpError("Unauthorised action.", 401);
     return next(error);
   }
-}
+};
 
 module.exports = checkPermission;

030c57cfc40b325ca7e59ea55647b6afec328cf2
bnj997 <bnj997@gmail.com>
2023-08-17 12:41:28 +1000

    minor changesa

--- controllers/orders-controller.js
+++ controllers/orders-controller.js
@@ -1,85 +1,88 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const { v4: uuidv4 } = require('uuid');
-const { validationResult } = require('express-validator');
-const mongoose = require('mongoose');
-const HttpError = require('../models/http-error');
-const checkPermission = require('../utils/check-permission')
-const sendEnquiryForm = require('../utils/send-enquiry')
-const sendOrderForm = require('../utils/send-order')
+const { v4: uuidv4 } = require("uuid");
+const { validationResult } = require("express-validator");
+const mongoose = require("mongoose");
+const HttpError = require("../models/http-error");
+const checkPermission = require("../utils/check-permission");
+const sendEnquiryForm = require("../utils/send-enquiry");
+const sendOrderForm = require("../utils/send-order");
 
-const Order = require('../models/order');
-const Orderline = require('../models/order-line');
-const User = require('../models/user')
-
+const Order = require("../models/order");
+const Orderline = require("../models/order-line");
+const User = require("../models/user");
 
 const getOrders = async (req, res, next) => {
   checkPermission(req.userData.username, next);
   let orders;
   try {
-    orders = await Order.find().populate('hq').populate('branch');
+    orders = await Order.find().populate("hq").populate("branch");
   } catch (err) {
     const error = new HttpError(
-      'Fetching Orders failed, please try again later.',
+      "Fetching Orders failed, please try again later.",
       500
     );
     return next(error);
   }
-  res.json({ orders: orders.map(order => order.toObject({ getters: true })) });
+  res.json({
+    orders: orders.map((order) => order.toObject({ getters: true })),
+  });
 };
 
-
-
 const getOrdersByUser = async (req, res, next) => {
-  const userId = req.params.uid
+  const userId = req.params.uid;
   let orders;
   try {
-    orders = await Order.find({user : userId}).populate('hq').populate('branch');
+    orders = await Order.find({ user: userId })
+      .populate("hq")
+      .populate("branch");
   } catch (err) {
     const error = new HttpError(
-      'Fetching Orders failed, please try again later.',
+      "Fetching Orders failed, please try again later.",
       500
     );
     return next(error);
   }
-  res.json({ orders: orders.map(order => order.toObject({ getters: true })) });
+  res.json({
+    orders: orders.map((order) => order.toObject({ getters: true })),
+  });
 };
 
 const getOrderByID = async (req, res, next) => {
-  const orderID = req.params.oid
+  const orderID = req.params.oid;
   let order;
   try {
-    order = await Order.findById(orderID).populate('hq').populate('branch');
+    order = await Order.findById(orderID).populate("hq").populate("branch");
   } catch (err) {
     const error = new HttpError(
-      'Fetching Order failed, could not find Order.',
+      "Fetching Order failed, could not find Order.",
       500
     );
     return next(error);
   }
 
-  res.json({order : order.toObject({ getters: true }) });
+  res.json({ order: order.toObject({ getters: true }) });
 };
-
 
 const createOrder = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return next(
-      new HttpError('Invalid inputs passed, please check your data.', 422)
+      new HttpError("Invalid inputs passed, please check your data.", 422)
     );
   }
-  const { _id, user, info} = req.body;
+  const { _id, user, info } = req.body;
 
-  var userHQ = await User.findById(user).populate('hq')
-  userHQ = userHQ.hq._id
-  var userBranch = await User.findById(user).populate('branch')
-  userBranch = userBranch.branch._id
+  var userHQ = await User.findById(user).populate("hq");
+  userHQ = userHQ.hq._id;
+  var userBranch = await User.findById(user).populate("branch");
+  userBranch = userBranch.branch._id;
 
-
-  var aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Melbourne"});
+  var aestTime = new Date().toLocaleString("en-US", {
+    timeZone: "Australia/Melbourne",
+  });
   var date = new Date(aestTime);
-  var dateString = `${date.toDateString()}, ${date.toLocaleTimeString()}`
+  var dateString = `${date.toDateString()}, ${date.toLocaleTimeString()}`;
 
   const createdOrder = new Order({
     _id,
@@ -94,11 +97,11 @@ const createOrder = async (req, res, next) => {
   try {
     const sess = await mongoose.startSession();
     sess.startTransaction();
-    await createdOrder.save(); 
+    await createdOrder.save();
     await sess.commitTransaction();
   } catch (err) {
     const error = new HttpError(
-      'Creating Order failed, please try again.',
+      "Creating Order failed, please try again.",
       500
     );
     return next(error);
@@ -106,13 +109,84 @@ const createOrder = async (req, res, next) => {
   res.status(201).json({ order: createdOrder.toObject({ getters: true }) });
 };
 
+const mergeOrders = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
+  }
+
+  const sigIds = [
+    "855270f0-07ad-4c4d-9bbd-64d93dde3964",
+    "630ffad9-6923-1a3f-0831-9f2b87dfap32",
+    "78d63670-f7ea-4ab5-bb3c-1ba3cb3e8642",
+    "c880d530-4436-45ba-a3d7-c9f69f665db3",
+    "a7e8ce26-2f6f-4375-bf70-900ca74ce900",
+    "36cf4edb-54ff-4cdc-8bbf-37cf49bae65a",
+    "24acf03a-4045-4b3d-920c-b18eeffeb52b",
+    "956cd602-465d-44af-aeea-556e4f9c9af6",
+    "baa17ad3-01ad-40b4-8ebe-a254421a19f1",
+    "630ffb3f-6923-1a3f-0831-9f2c-8ap310a7er1g",
+    "630ff9f9-69fd-74f1-0831-9f2af835js124",
+    "85ddd88f-636b-458a-95f0-f8396345016a",
+    "8bb9134a-c4e9-4e5e-aa7b-af15242fdeca",
+    "71b6a583-9e34-4461-afbb-a557df60b163",
+  ];
+
+  let ordersToChange;
+  try {
+    ordersToChange = await Order.find({ _id: { $in: sigIds } });
+  } catch (err) {
+    const error = new HttpError(
+      "Merging orders failed, please try again.",
+      500
+    );
+    return next(error);
+  }
+
+  try {
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+
+    // Using updateMany to update all orders with ids in sigIds
+    await Order.updateMany(
+      { _id: { $in: sigIds } },
+      { hq: "1111-1111-1111" },
+      { session: sess }
+    );
+
+    await sess.commitTransaction();
+  } catch (err) {
+    const error = new HttpError(
+      "Creating Order failed, please try again.",
+      500
+    );
+    return next(error);
+  }
+
+  // Optionally, you can send the updated orders back in the response
+  try {
+    ordersToChange = await Order.find({ _id: { $in: sigIds } });
+  } catch (err) {
+    const error = new HttpError(
+      "Fetching updated orders failed, please try again.",
+      500
+    );
+    return next(error);
+  }
+
+  res.status(201).json({
+    orders: ordersToChange.map((order) => order.toObject({ getters: true })),
+  });
+};
 
 const deleteOrder = async (req, res, next) => {
   const orderId = req.params.oid;
 
   let order;
   try {
-    order = await Order.findById(orderId).populate('orderlines');
+    order = await Order.findById(orderId).populate("orderlines");
   } catch (err) {
     const error = new HttpError(
       `Something went wrong, could not delete order. + ${err}`,
@@ -122,15 +196,15 @@ const deleteOrder = async (req, res, next) => {
   }
 
   if (!order) {
-    const error = new HttpError('Could not find order for this id.', 404);
+    const error = new HttpError("Could not find order for this id.", 404);
     return next(error);
   }
 
   try {
     const sess = await mongoose.startSession();
     sess.startTransaction();
-    await order.remove({session: sess});
-    await Orderline.deleteMany({order: orderId})
+    await order.remove({ session: sess });
+    await Orderline.deleteMany({ order: orderId });
     await sess.commitTransaction();
   } catch (err) {
     const error = new HttpError(
@@ -139,63 +213,57 @@ const deleteOrder = async (req, res, next) => {
     );
     return next(error);
   }
-  
-  res.status(200).json({ message: 'Deleted order' });
-};
 
+  res.status(200).json({ message: "Deleted order" });
+};
 
 const sendEnquiry = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return next(
-      new HttpError('Invalid inputs passed, please check your data.', 422)
+      new HttpError("Invalid inputs passed, please check your data.", 422)
     );
   }
 
-  const {name, email, organisation , message} = req.body;
+  const { name, email, organisation, message } = req.body;
 
   try {
     sendEnquiryForm(name, email, organisation, message);
   } catch (err) {
-    const error = new HttpError(
-      'Sending form failed, please try again.',
-      500
-    );
+    const error = new HttpError("Sending form failed, please try again.", 500);
     return next(error);
   }
   res.status(201).send(true);
 };
 
 const sendOrder = async (req, res, next) => {
-  const order = req.params.oid
-  const {cart} = req.body;
+  const order = req.params.oid;
+  const { cart } = req.body;
   let thisOrder;
   try {
-    thisOrder = await Order.findById(order).populate('user').populate('branch').populate('hq');
+    thisOrder = await Order.findById(order)
+      .populate("user")
+      .populate("branch")
+      .populate("hq");
   } catch (err) {
     const error = new HttpError(
-      'Fetching Order failed, could not find Order.',
+      "Fetching Order failed, could not find Order.",
       500
     );
     return next(error);
   }
   try {
-    sendOrderForm(thisOrder, cart)
+    sendOrderForm(thisOrder, cart);
   } catch (err) {
-    const error = new HttpError(
-      'Sending order failed, please try again.',
-      500
-    );
+    const error = new HttpError("Sending order failed, please try again.", 500);
     return next(error);
   }
   res.status(201).send(true);
-}
-
-
-
+};
 
 exports.createOrder = createOrder;
 exports.getOrdersByUser = getOrdersByUser;
+exports.mergeOrders = mergeOrders;
 exports.getOrders = getOrders;
 exports.deleteOrder = deleteOrder;
 exports.getOrderByID = getOrderByID;
--- routes/orders-routes.js
+++ routes/orders-routes.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const ordersController = require('../controllers/orders-controller');
-const checkAuth = require('../middleware/check-auth');
+const express = require("express");
+const ordersController = require("../controllers/orders-controller");
+const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
-router.post('/enquiry', ordersController.sendEnquiry);
+router.post("/enquiry", ordersController.sendEnquiry);
 router.use(checkAuth);
-router.post('/makeorder/:oid', ordersController.sendOrder);
-router.get('/user/:uid', ordersController.getOrdersByUser);
-router.get('/:oid', ordersController.getOrderByID);
-router.delete('/:oid', ordersController.deleteOrder);
-router.get('/', ordersController.getOrders);
-router.post('/', ordersController.createOrder);
-
-
+router.post("/makeorder/:oid", ordersController.sendOrder);
+router.post("/mergeorders", ordersController.mergeOrder);
+router.get("/user/:uid", ordersController.getOrdersByUser);
+router.get("/:oid", ordersController.getOrderByID);
+router.delete("/:oid", ordersController.deleteOrder);
+router.get("/", ordersController.getOrders);
+router.post("/", ordersController.createOrder);
 
 module.exports = router;
